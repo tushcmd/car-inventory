5b39fdd7dc5ed0668b992682f389ed2eec9620de
tushcmd <muturidavid854@gmail.com>
2024-05-11 04:05:17 +0300

    added config.js in backend

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 8080;
+
+export const mongoDBURL =
+    'mongodb+srv://muturidavid854:wC7KjgePW4rYJOda@cluster0.vpmvgpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
